74ec6f27c34b601a3e226d3b6b4bc6729950321b
bokovm <mbvlog07@gmail.com>
2025-09-19 16:28:23 +0300

    add UserBotHandlerMasseges propertires

--- src/main/java/com/taipan/userbot/handler/CallbackQueryHandler.java
+++ src/main/java/com/taipan/userbot/handler/CallbackQueryHandler.java
@@ -1,21 +1,30 @@
 package com.taipan.userbot.handler;
 
 import com.taipan.userbot.bot.BotCore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
+@PropertySource("classpath:callback_query.properties")
 @Service
 public class CallbackQueryHandler {
+
+    @Value("${button.clicked:Вы нажали кнопку}")
+    private String buttonMessage;
 
     public void handle(Update update, BotCore bot) {
         String data = update.getCallbackQuery().getData();
         long chatId = update.getCallbackQuery().getMessage().getChatId();
 
-        // Пока просто отвечаем, но можно интегрировать с BotService
+        // Если нужно вставить data в сообщение, можно использовать replace или String.format
+        String text = buttonMessage.replace("{0}", data);
+        // или: String text = String.format(buttonMessage, data);
+
         SendMessage response = SendMessage.builder()
                 .chatId(String.valueOf(chatId))
-                .text("Вы нажали кнопку: `" + data + "`")
+                .text(text)
                 .parseMode("Markdown")
                 .build();
 
