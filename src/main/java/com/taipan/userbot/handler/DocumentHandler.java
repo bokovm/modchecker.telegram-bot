feb9be413ee6c49bfc97d4b2cd6c47e61d1f9282
bokovm <mbvlog07@gmail.com>
2025-09-20 22:34:33 +0300

    fix: add detailed error logging in DocumentHandler with file context

--- src/main/java/com/taipan/userbot/handler/DocumentHandler.java
+++ src/main/java/com/taipan/userbot/handler/DocumentHandler.java
@@ -1,12 +1,14 @@
 package com.taipan.userbot.handler;
 
 import com.taipan.userbot.bot.BotCore;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Slf4j
 @Service
 @PropertySource("classpath:UserBotHandlerMessages/document_handler.properties")
 public class DocumentHandler {
@@ -28,7 +30,7 @@ public class DocumentHandler {
         try {
             bot.execute(response);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Ошибка обработки файла {} от пользователя {}, дополнительная информация - {}", fileName, chatId, e);
         }
     }
 }
